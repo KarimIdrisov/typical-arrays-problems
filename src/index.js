a507b6e40e19d864b2410939525534ca4dbad993
Karim Idrisov <karim.idrisow2016@gmail.com>
2021-08-11 12:33:38 +1000

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+
+    let min = Infinity;
+    for (let i = 0; i < array.length; i = i + 1) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+
+    let max = -Infinity;
+    for (let i = 0; i < array.length; i = i + 1) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+
+    let sum = array.reduce((a, b) => a + b, 0);
+
+    return sum / array.length;
 }
